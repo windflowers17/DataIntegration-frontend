4abb51eb72da0abc6a9ff4c0aeaf5ffb7f9fed88
wenglanfeng <201870192@smail.nju.edu.cn>
2023-03-27 13:08:04 +0800

    test

--- src/network/courses/index.js
+++ src/network/courses/index.js
@@ -1,6 +1,7 @@
 import request from "@/network/request";
 const API = require("@/apis")
 
+
 // heart_thought
 export const getAllCoursesFromC = config => request._get(API.ALL_COURSES_FROM_C, config);
 
