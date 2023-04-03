6608bc8224d0fbda259044bd14c401d9a70f0668
wenglanfeng <201870192@smail.nju.edu.cn>
2023-04-03 13:18:49 +0800

    修改CoursesCView

--- src/view/CoursesCView.vue
+++ src/view/CoursesCView.vue
@@ -75,7 +75,7 @@
 
 <script>
 import Head from '@/view/Head.vue'
-import { getAllCoursesFromC, selectCourseFromC, getAllCourses } from '@/network/courses/index.js';
+import { getAllCoursesFromC, selectCourseFromC } from '@/network/courses/index.js';
 const xml2js = require('xml2js');
 
 export default {
@@ -116,25 +116,24 @@ export default {
         this.loadCoursesFromC();
       }
       else if (selectedInstitude === 'ABC') {
-        //TODO: ALL COURSES FROM A B C
-          getAllCourses().then(res => {
-            this.courseTable = [];
-            let xmlDoc = new DOMParser().parseFromString(res, 'text/xml');
-            let courses = xmlDoc.getElementsByTagName('课程');
-            for (let i = 0; i < courses.length; ++i) {
-              let course = courses[i];
-              // console.log(course);
-              let item = {
-                number: course.childNodes[0].innerHTML,//编号
-                name: course.childNodes[1].innerHTML,//名称
-                time: course.childNodes[2].innerHTML,//课时
-                credit: course.childNodes[3].innerHTML,//学分
-                teacher: course.childNodes[4].innerHTML,//老师
-                place: course.childNodes[5].innerHTML,
-              }
-              this.courseTable.push(item);
+        getAllCourses().then(res => {
+          this.courseTable = [];
+          let xmlDoc = new DOMParser().parseFromString(res, 'text/xml');
+          let courses = xmlDoc.getElementsByTagName('课程');
+          for (let i = 0; i < courses.length; ++i) {
+            let course = courses[i];
+            // console.log(course);
+            let item = {
+              number: course.childNodes[0].innerHTML,//编号
+              name: course.childNodes[1].innerHTML,//名称
+              time: course.childNodes[2].innerHTML,//课时
+              credit: course.childNodes[3].innerHTML,//学分
+              teacher: course.childNodes[4].innerHTML,//老师
+              place: course.childNodes[5].innerHTML,
             }
-          })
+            this.courseTable.push(item);
+          }
+        })
       }
     },
     /**
