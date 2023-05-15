2e2af07087be74c268f4413ab76521cd5434f930
wenglanfeng <201870192@smail.nju.edu.cn>
2023-05-15 21:32:39 +0800

    修改字体大小

--- src/view/charts/CoursesNumOfDiffCredits.vue
+++ src/view/charts/CoursesNumOfDiffCredits.vue
@@ -24,7 +24,7 @@
     <div style="width:600px;height:400px;">
       <div
         id="courseNumOfDifferentCredits"
-        style="width:100%;height:278px;float:left;"
+        style="width:100%;height:400px;float:left;"
       ></div>
     </div>
   </div>
@@ -35,7 +35,7 @@ export default {
   data() {
     return {
       chart: '',
-      opinion: ['2学分', '3学分', '4学分', '5学分'],
+      opinion: ['1学分' ,'2学分', '3学分', '4学分', '5学分'],
       opinionData: [
         { value: 1, name: '1学分', itemStyle: '#FF6384' },
         { value: 9, name: '2学分', itemStyle: '#36A2EB' },
@@ -64,12 +64,16 @@ export default {
         },
         tooltip: {
           trigger: 'item',
-          formatter: '{a} <br/>{b} : {c} ({d}%)'
+          formatter: '{a} <br/>{b} : {c} ({d}%)',
+          fontSize: 16,
         },
         legend: {
           orient: 'vertical',
           bottom: 'bottom',
-          data: this.opinion
+          data: this.opinion,
+          textStyle: {
+            fontSize: 16,
+          }
         },
         series: [
           {
@@ -84,11 +88,18 @@ export default {
                 shadowOffsetX: 0,
                 shadowColor: 'rgba(0, 0, 0, 0.5)'
               },
-              color: function (params) {
+              normal: {
+                label: {
+                  textStyle: {
+                    fontSize: 16
+                  }
+                },
+                color: function (params) {
                 // 自定义颜色
-                var colorList = ['#FF6384', '#1ab394','#36A2EB','#FFCE56','#4BC0C0','#E84393']
+                var colorList = ['#FF6384', '#1ab394', '#36A2EB', '#FFCE56', '#4BC0C0', '#E84393']
                 return colorList[params.dataIndex]
               }
+              },
             }
           }
         ]
